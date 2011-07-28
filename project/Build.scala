09e9d962a99c5e13c3a243030bd9a5281fffb08f
Kris Nuttycombe <kris@hylotech.com>
2011-07-28 09:15:44 -0600

    Updated scala to 2.9.1-RC1

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import AltDependency._
 object RosettaJsonBuild extends Build {
   val buildOrganization = "com.reportgrid"
   val buildVersion = "0.3.1"
-  val buildScalaVersion = "2.9.0-1"
+  val buildScalaVersion = "2.9.1.RC1"
   
   val blueeyes = GitAltDependency(_: java.io.File, file("../blueeyes"), RootProject(uri("git://github.com/jdegoes/blueeyes")))
 
@@ -16,10 +16,10 @@ object RosettaJsonBuild extends Build {
       scalaVersion := buildScalaVersion,
       scalacOptions ++= Seq("-deprecation", "-unchecked"),
       libraryDependencies ++= Seq(
-        "net.databinder"          %% "dispatch-http-json"  % "0.8.3"   % "provided",
-        "net.liftweb"             %% "lift-json"           % "2.4-M2"  % "provided",
-        "org.scala-tools.testing" %% "scalacheck"          % "1.9"     % "test",
-        "org.scala-tools.testing" %% "specs"               % "1.6.8"   % "test"
+        "net.databinder"          % "dispatch-http-json_2.9.0-1"  % "0.8.3"   % "provided",
+        "net.liftweb"             % "lift-json_2.9.0-1"           % "2.4-M2"  % "provided",
+        "org.scala-tools.testing" % "scalacheck_2.9.0-1"          % "1.9"     % "test",
+        "org.scala-tools.testing" % "specs_2.9.0-1"               % "1.6.8"   % "test"
       )
     ) dependsOnAlt(blueeyes(base)) 
     
