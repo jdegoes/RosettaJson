83dc693d3b6d841c4307a7b48164f21f62c1e18e
Kris Nuttycombe <kris@hylotech.com>
2011-09-22 12:03:20 -0600

    Updated dependencies.

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import AltDependency._
 
 object RosettaJsonBuild extends Build {
   val buildOrganization = "com.reportgrid"
-  val buildVersion = "0.3.3"
+  val buildVersion = "0.3.4"
   val buildScalaVersion = "2.9.1"
   
   val blueeyes = GitAltDependency(_: java.io.File, file("../blueeyes"), RootProject(uri("git://github.com/jdegoes/blueeyes")))
@@ -16,10 +16,10 @@ object RosettaJsonBuild extends Build {
       scalaVersion := buildScalaVersion,
       scalacOptions ++= Seq("-deprecation", "-unchecked"),
       libraryDependencies ++= Seq(
-        "net.databinder"          % "dispatch-http-json_2.9.0-1"  % "0.8.3"   % "provided",
-        "net.liftweb"             % "lift-json_2.9.0-1"           % "2.4-M2"  % "provided" intransitive(),
-        "org.scala-tools.testing" % "scalacheck_2.9.0-1"          % "1.9"     % "test",
-        "org.scala-tools.testing" % "specs_2.9.0-1"               % "1.6.8"   % "test"
+        "net.databinder"          %% "dispatch-http-json"  % "0.8.5"   % "provided",
+        "net.liftweb"             %% "lift-json"           % "2.4-M4"  % "provided" intransitive(),
+        "org.scala-tools.testing" %% "scalacheck"          % "1.9"     % "test",
+        "org.scala-tools.testing" %% "specs"               % "1.6.9"   % "test"
       )
     ) dependsOnAlt(blueeyes(base)) 
     
