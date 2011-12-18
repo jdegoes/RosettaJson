3b13d70e76cc80b72dff689d17f0bc1863e07c88
Derek Chen-Becker <derek@reportgrid.com>
2011-12-18 09:06:23 -0700

    Publish to internal repo

--- project/Build.scala
+++ project/Build.scala
@@ -15,12 +15,20 @@ object RosettaJsonBuild extends Build {
       version      := buildVersion,
       scalaVersion := buildScalaVersion,
       scalacOptions ++= Seq("-deprecation", "-unchecked"),
+      resolvers ++= Seq("ReportGrid repo" at            "http://devci01.reportgrid.com:8081/content/repositories/releases",
+			"ReportGrid snapshot repo" at   "http://devci01.reportgrid.com:8081/content/repositories/snapshots"),
       libraryDependencies ++= Seq(
         "net.databinder"          %% "dispatch-http-json"  % "0.8.5"   % "provided",
         "net.liftweb"             %% "lift-json"           % "2.4-M4"  % "provided" intransitive(),
         "org.scala-tools.testing" %% "scalacheck"          % "1.9"     % "test",
         "org.scala-tools.testing" %% "specs"               % "1.6.9"   % "test"
-      )
+      ),
+      publishTo <<= (version) { version: String =>
+	val nexus = "http://nexus.reportgrid.com/content/repositories/"
+	if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus+"snapshots/") 
+	else                                   Some("releases"  at nexus+"releases/")
+      },
+      credentials := Credentials(Path.userHome / ".ivy2" / ".rgcredentials") :: Nil
     ) dependsOnAlt(blueeyes(base)) 
     
     rosettaJson :: Nil
