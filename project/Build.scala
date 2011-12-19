abd7ff976c506074fd3bf123a199da6f6b418bdb
Derek Chen-Becker <derek@reportgrid.com>
2011-12-19 10:43:29 -0700

    Fixed to use condeps instead of xsbt-alt-deps. Also publish to internal nexus

--- project/Build.scala
+++ project/Build.scala
@@ -1,35 +1,38 @@
 import sbt._
 import Keys._
-import AltDependency._
 
 object RosettaJsonBuild extends Build {
   val buildOrganization = "com.reportgrid"
   val buildVersion = "0.3.4"
   val buildScalaVersion = "2.9.1"
-  
-  val blueeyes = GitAltDependency(_: java.io.File, file("../blueeyes"), RootProject(uri("git://github.com/jdegoes/blueeyes")))
+
+  val blueeyesDeps = com.samskivert.condep.Depends( 
+    ("blueeyes",    null, "com.reportgrid" %% "blueeyes"     % "0.5.0-SNAPSHOT")
+  )  
 
   override def projectDefinitions(base: File) = {
-    val rosettaJson: Project = Project("rosetta-json", file(".")) settings(
+    val rosettaJson: Project = blueeyesDeps.addDeps(Project("rosetta-json", file(".")).settings(
       organization := buildOrganization,
       version      := buildVersion,
       scalaVersion := buildScalaVersion,
       scalacOptions ++= Seq("-deprecation", "-unchecked"),
-      resolvers ++= Seq("ReportGrid repo" at            "http://devci01.reportgrid.com:8081/content/repositories/releases",
-			"ReportGrid snapshot repo" at   "http://devci01.reportgrid.com:8081/content/repositories/snapshots"),
-      libraryDependencies ++= Seq(
+      libraryDependencies ++= (blueeyesDeps.libDeps ++ Seq(
         "net.databinder"          %% "dispatch-http-json"  % "0.8.5"   % "provided",
         "net.liftweb"             %% "lift-json"           % "2.4-M4"  % "provided" intransitive(),
         "org.scala-tools.testing" %% "scalacheck"          % "1.9"     % "test",
         "org.scala-tools.testing" %% "specs"               % "1.6.9"   % "test"
-      ),
+      )),
+      resolvers ++= Seq("ReportGrid repo" at                   "http://nexus.reportgrid.com/content/repositories/releases",
+			"ReportGrid snapshot repo" at          "http://nexus.reportgrid.com/content/repositories/snapshots",
+			"ReportGrid public repo" at            "http://nexus.reportgrid.com/content/repositories/public-releases",
+			"ReportGrid public snapshot repo" at   "http://nexus.reportgrid.com/content/repositories/public-snapshots"),
       publishTo <<= (version) { version: String =>
-	val nexus = "http://nexus.reportgrid.com/content/repositories/"
-	if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus+"snapshots/") 
-	else                                   Some("releases"  at nexus+"releases/")
+        val nexus = "http://nexus.reportgrid.com/content/repositories/"
+        if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus+"snapshots/") 
+        else                                   Some("releases"  at nexus+"releases/")
       },
-      credentials := Credentials(Path.userHome / ".ivy2" / ".rgcredentials") :: Nil
-    ) dependsOnAlt(blueeyes(base)) 
+      credentials += Credentials(Path.userHome / ".ivy2" / ".rgcredentials")
+    ))
     
     rosettaJson :: Nil
   }
