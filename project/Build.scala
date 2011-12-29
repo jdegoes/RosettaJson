5684fba7355c072c82897296fc451de9f0921cc5
Kris Nuttycombe <kris@reportgrid.com>
2011-12-29 12:11:07 -0700

    Updated to sbt 0.11.x build

--- project/Build.scala
+++ project/Build.scala
@@ -1,31 +1,34 @@
 import sbt._
 import Keys._
-import AltDependency._
 
 object RosettaJsonBuild extends Build {
-  val buildOrganization = "com.reportgrid"
-  val buildVersion = "0.3.4"
-  val buildScalaVersion = "2.9.1"
-  
-  val blueeyes = GitAltDependency(_: java.io.File, file("../blueeyes"), RootProject(uri("git://github.com/jdegoes/blueeyes")))
+  val nexusSettings = Defaults.defaultSettings ++ Seq (
+    resolvers ++= Seq("ReportGrid repo"          at   "http://nexus.reportgrid.com/content/repositories/releases",
+                      "ReportGrid repo (public)" at   "http://nexus.reportgrid.com/content/repositories/public-releases",
+                      "ReportGrid snapshot repo"          at   "http://nexus.reportgrid.com/content/repositories/snapshots",
+                      "ReportGrid snapshot repo (public)" at   "http://nexus.reportgrid.com/content/repositories/public-snapshots"),
 
-  override def projectDefinitions(base: File) = {
-    val rosettaJson: Project = Project("rosetta-json", file(".")) settings(
-      organization := buildOrganization,
-      version      := buildVersion,
-      scalaVersion := buildScalaVersion,
-      scalacOptions ++= Seq("-deprecation", "-unchecked"),
-      libraryDependencies ++= Seq(
-        "net.databinder"          %% "dispatch-http-json"  % "0.8.5"   % "provided",
-        "net.liftweb"             %% "lift-json"           % "2.4-M4"  % "provided" intransitive(),
-        "org.scala-tools.testing" %% "scalacheck"          % "1.9"     % "test",
-        "org.scala-tools.testing" %% "specs"               % "1.6.9"   % "test"
-      )
-    ) dependsOnAlt(blueeyes(base)) 
-    
-    rosettaJson :: Nil
-  }
+    credentials += Credentials(Path.userHome / ".ivy2" / ".rgcredentials")
+  )
+
+  lazy val rosettaJson: Project = Project("rosetta-json", file(".")) settings(nexusSettings ++ Seq(
+    organization := "com.reportgrid",
+    version      := "0.3.5",
+    scalaVersion := "2.9.1",
+    scalacOptions ++= Seq("-deprecation", "-unchecked"),
+    libraryDependencies ++= Seq(
+      "com.reportgrid"          %% "blueeyes"            % "0.5.1",
+      "net.databinder"          %% "dispatch-http-json"  % "0.8.5"   % "provided",
+      "net.liftweb"             %% "lift-json"           % "2.4-M4"  % "provided" intransitive(),
+      "org.scala-tools.testing" %% "scalacheck"          % "1.9"     % "test",
+      "org.scala-tools.testing" %% "specs"               % "1.6.9"   % "test"
+    ),
+    publishTo <<= (version) { version: String =>
+      val nexus = "http://nexus.reportgrid.com/content/repositories/public-"
+      if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus+"snapshots/") 
+      else                                   Some("releases"  at nexus+"releases/")
+    }
+  ): _*)
 }
-
 
 // vim: set ts=4 sw=4 et:
--- project/plugins/project/Build.scala
+++
@@ -1,7 +0,0 @@
-import sbt._
-object PluginDef extends Build {
-  lazy val root = Project("plugins", file(".")) dependsOn (altDep)
-  lazy val altDep = RootProject(uri("git://github.com/reportgrid/xsbt-alt-deps")) 
-}
-
-// vim: set ts=4 sw=4 et:
